f1bb96f0158eed1e936024846bb55c3f060b21ac
Practicum <practicum@ya.ru>
2023-07-30 20:50:07 +0300

    Первый коммит

---
+++ app/src/main/java/com/practium/gittest/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.practium.gittest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        // добавлено 1 изменение ветка Мастер
+    }
+}
